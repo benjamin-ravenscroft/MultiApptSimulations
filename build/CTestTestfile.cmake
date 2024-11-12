29aab40430c063d8b7a483ef328972897386525f
benjamin-ravenscroft <benjamin.ravenscroft@gmail.com>
2024-11-12 22:44:20 +0000

    Removed build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benja/kidsAbility/MultiApptSimulations
-# Build directory: /home/benja/kidsAbility/MultiApptSimulations/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("extern/cxxopts")
--- build/extern/cxxopts/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benja/kidsAbility/MultiApptSimulations/extern/cxxopts/include
-# Build directory: /home/benja/kidsAbility/MultiApptSimulations/build/extern/cxxopts/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
